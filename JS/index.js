80fe0279c49eaea55a3f3663d5f72669835a7a2e
Dalvishubh <sithdalvi123@gmail.com>
2023-06-19 23:48:27 +0530

    CSS JS html added

--- JS/index.js
+++ JS/index.js
@@ -21,48 +21,70 @@ else
     alert("NA");
 }
 
-
 console.log(value);
-
 getWinner();
-
-value[index]='';
-
-
 })
+
 
 function getVal()
 {
     return (yes)? "X":"O";
-}
+}   
 
+let winnerIndex=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6] 
+];
 
-function getWinner()
-{
-    if((value[0]==value[1] && value[1]==value[2]) || (value[3]==value[4] && value[4]==value[5]) || (value[6]==value[7] && value[7]==value[8]))
+function getWinner(){
+    for(let i=0; i<winnerIndex.length; i++)
+    { 
+    [a,b,c]=winnerIndex[i]
+    if(value[a]==value[b] && value[b]==value[c])
     {
-    
-        getanswer()
-    
-    }
-    
-
-    if((value[0]==value[3] && value[3]==value[6]) || (value[1]==value[4] && value[4]==value[7]) || (value[2]==value[5] && value[5]==value[8]))
-    {
-    
-        getanswer()
-    
-    }
-
-
-    if((value[0]==value[4] && value[4]==value[8]) || (value[2]==value[4] && value[4]==value[6]))
-    {
-    
-        getanswer()
-    
+        getanswer(a,b,c);
+        console.log(a,b,c);
+        break;
     }
 
 }
+}
+
+
+
+
+// function getWinner()
+// {
+//     if((value[0]==value[1] && value[1]==value[2]) || (value[3]==value[4] && value[4]==value[5]) || (value[6]==value[7] && value[7]==value[8]))
+//     {
+    
+//         getanswer()
+        
+//     }
+    
+
+//      else if((value[0]==value[3] && value[3]==value[6]) || (value[1]==value[4] && value[4]==value[7]) || (value[2]==value[5] && value[5]==value[8]))
+//     {
+    
+//         getanswer()
+        
+//     }
+
+
+//      else if((value[0]==value[4] && value[4]==value[8]) || (value[2]==value[4] && value[4]==value[6]))
+//     {
+    
+//         getanswer()
+    
+//     }
+
+// }
 
 player1=document.querySelector('#user1').value;
 player2=document.querySelector('#user2').value;
@@ -102,31 +124,35 @@ document.querySelector('#reset').addEventListener('click',function(){
 })
 
 
-function getanswer()
+function getanswer(x,y,z)
 {
 
      if(yes){
-     document.querySelector('.play2').innerHTML=player2count;
-     player2count++;
-     alert("player2count") 
+         player2count++;
+         document.querySelector('.play2').innerHTML=player2count;
+     alert("player2count"); 
     }
      else{
-       document.querySelector('.play1').innerHTML= player1count;
-      player1count++; 
-     alert("player1count")
+         player1count++; 
+         document.querySelector('.play1').innerHTML= player1count;
+     alert("player1count");
     }
-    
-}
+     document.getElementById(x).style='background-color: grey;'
+     document.getElementById(y).style='background-color: grey;'
+     document.getElementById(z).style='background-color: grey;'
+     value=[];    
+    }
 
 document.querySelector('.reset1').addEventListener('click',function(){
    var data=document.querySelectorAll('.td')
    for(let i=0; i<data.length; i++)
    {
             data[i].innerHTML="";
-   }
+            data[i].style.backgroundColor='black' 
+        }
    value=[1,0,1,0,1,0,0,1,0]
    console.log(value);
-   value=true;
+   yes=true;
 })
 
 
